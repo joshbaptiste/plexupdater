5ba2ff696a78e003137a2e8ff140456a0cd770e4
Reynold Chery <joshbaptiste@yahoo.com>
2020-05-14 04:34:23 +0000

    bad bugs fixed and tested..

--- plex_updater_docker.sh
+++ plex_updater_docker.sh
@@ -1,20 +1,24 @@
-#!/usr/bin/env bash -x
-new_version=$(curl -sN https://aur.archlinux.org/packages/plex-media-server-plexpass | grep -i "package details" | grep -Eo "[0-9][-.0-9]+[0-9]")
+#!/usr/bin/env bash
+
+url="https://aur.archlinux.org/packages/plex-media-server-plexpass"
+new_version=$(curl -sN "$url" | grep -i "package details" | grep -Eo "[0-9][-.0-9]+[0-9]")
 media="/mnt/superfree/media"
+media2="/mnt/superfree2"
 name="plex"
 
 if [[ -n $new_version ]]
 then
-    if current_version=$(docker container ls --filter "name=$name" --format "{{.Image}}" | grep "$new_version")
+    # If current container version matches we have the latest
+    if docker container ls --filter "name=$name" --format "{{.Image}}" | grep -q "$new_version"
     then
-         exit 0 
+         exit 0
     else
         tag="local:plex-$new_version"
         echo "Version mismatch.. upgrading Plex"
         docker container stop $name
         docker container rm $name
-        docker image build  --file Dockerfile --no-cache --tag $tag
-        docker container run --detach -it --net=host --name $name -v /mnt/plex:/mnt -v $media:$media -v /mnt/superfree2:/mnt/superfree2 $tag
+        docker image build  --file Dockerfile --no-cache --tag $tag .
+        docker container run --detach -it --net=host --name $name -v /mnt/plex:/mnt -v $media:$media -v $media2:$media2 $tag
     fi
 fi
 
